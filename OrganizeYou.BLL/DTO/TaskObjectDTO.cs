475f5fdd6dafed1fe927a3fb9cdd843e7917e334
m3mph17 <ez4genius@gmail.com>
2023-09-14 00:11:50 +0300

    Base three-layer structure

---
+++ OrganizeYou.BLL/DTO/TaskObjectDTO.cs
@@ -0,0 +1,19 @@
+﻿using OrganizeYou.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizeYou.BLL.DTO
+{
+    public class TaskObjectDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Completion { get; set; }
+        public Status Status { get; set; }
+    }
+}
---
+++ OrganizeYou.DAL/Entities/TaskObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizeYou.DAL.Entities
+{
+    public class TaskObject
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Completion { get; set; }
+        public Status Status { get; set; }
+    }
+}
