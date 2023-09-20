f1c67ff25a5a728a0d8173130bd8e2aa94e22919
m3mph17 <ez4genius@gmail.com>
2023-09-20 10:02:02 +0300

    pages for tasks

---
+++ OrganizeYou.Web/Views/Task/Mytask.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrganizeYou.Web.Views.Task
+{
+    public class MytaskModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ OrganizeYou.Web/Views/Task/UpdateTask.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrganizeYou.Web.Views.Task
+{
+    public class UpdateTaskModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
