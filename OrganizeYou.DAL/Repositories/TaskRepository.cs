aa383e62fe613d797aabadc3410282d4c040e02f
m3mph17 <ez4genius@gmail.com>
2023-09-20 09:48:12 +0300

    changed update method and added to other methods first(), toList()

--- OrganizeYou.DAL/Repositories/TaskRepository.cs
+++ OrganizeYou.DAL/Repositories/TaskRepository.cs
@@ -41,25 +41,43 @@ namespace OrganizeYou.DAL.Repositories
 
         public TaskObject Get(int id)
         {
-            TaskObject item = _db.Tasks.Find(id);
+            TaskObject item = _db.Tasks.Where(t => t.Id == id).First();
 
             if (item != null)
             {
                 var query = _db.Tasks.Include(t => t.Status);
-                return query.Where(t => t.Id == id).FirstOrDefault();
+                return query.Where(t => t.Id == id).First();
             }
             else
                 return null;
         }
 
+        public TaskObject Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerable<TaskObject> GetAll()
         {
-            return _db.Tasks.ToList();
+            var query = _db.Tasks.Include(t => t.Status); ;
+            return query.ToList();
         }
 
         public void Update(TaskObject item)
         {
-            _db.Entry(item).State = EntityState.Modified;
+            _db.ChangeTracker.Clear();
+            _db.Tasks.Update(item);
+            
+            // Same thing, just leaving it here =|
+
+            //TaskObject task = _db.Tasks.Where(t => t.Id == item.Id).First();
+            //task.Title = item.Title;
+            //task.Description = item.Description;
+            //task.Created = item.Created;
+            //task.Completion = item.Completion;
+            //task.Status = item.Status;
+
+            //_db.Entry(item).State = EntityState.Modified;
         }
     }
 }
