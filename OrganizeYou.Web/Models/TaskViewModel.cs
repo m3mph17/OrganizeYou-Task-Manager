b82e6f73c209486952d1f52df2daa56f991ca09a
m3mph17 <ez4genius@gmail.com>
2023-09-20 09:51:15 +0300

    convert method added. from StatusDTO to TaskObjectDTO

--- OrganizeYou.Web/Models/TaskViewModel.cs
+++ OrganizeYou.Web/Models/TaskViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace OrganizeYou.Web.Models
+﻿using OrganizeYou.BLL.DTO;
+using OrganizeYou.DAL.Entities;
+
+namespace OrganizeYou.Web.Models
 {
     public class TaskViewModel
     {
@@ -8,5 +11,21 @@
         public DateTime Created { get; set; }
         public DateTime Completion { get; set; }
         public string Status { get; set; }
+
+        public TaskObjectDTO Convert(StatusDTO status)
+        {
+            TaskObjectDTO dto = new TaskObjectDTO();
+            dto.Status = new Status();
+
+            dto.Id = Id;
+            dto.Title = Title;
+            dto.Description = Description;
+            dto.Created = Created;
+            dto.Completion = Completion;
+            dto.Status.Id = status.Id;
+            dto.Status.Name = status.Name;
+
+            return dto;
+        }
     }
 }
