b1c6aa81dd1e68a6057df2303213aeb06b0dc300
m3mph17 <ez4genius@gmail.com>
2023-09-20 09:59:48 +0300

    service for status

---
+++ OrganizeYou.BLL/Interfaces/IStatusService.cs
@@ -0,0 +1,17 @@
+﻿using OrganizeYou.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizeYou.BLL.Interfaces
+{
+    public interface IStatusService
+    {
+        StatusDTO GetStatus(int? id);
+        StatusDTO GetStatus(string name);
+        IEnumerable<StatusDTO> GetStatuses();
+        void Dispose();
+    }
+}
