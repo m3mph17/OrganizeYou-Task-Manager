5b9fc47bd2865942eb5d6bf1f0234c65596d95d9
m3mph17 <ez4genius@gmail.com>
2023-10-03 13:24:41 +0300

    user and role repository created

---
+++ OrganizeYou.DAL/Repositories/RoleRepository.cs
@@ -0,0 +1,55 @@
+﻿using OrganizeYou.DAL.EF;
+using OrganizeYou.DAL.Entities;
+using OrganizeYou.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizeYou.DAL.Repositories
+{
+    public class RoleRepository : IRepository<Role>
+    {
+        private readonly AppDbContext _db;
+        public RoleRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public void Create(Role item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Role> Find(Func<Role, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Role Get(int id)
+        {
+            Role userRole = _db.Roles.First(r => r.Name == "user");
+            return userRole;
+        }
+
+        public Role Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Role> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Role item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OrganizeYou.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrganizeYou.DAL.EF;
+using OrganizeYou.DAL.Entities;
+using OrganizeYou.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizeYou.DAL.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly AppDbContext _db;
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public void Create(User item)
+        {
+            item.Role = _db.Roles.First(r => r.Name == "user");
+            _db.Users.Add(item);   
+        }
+
+        public void Delete(int id)
+        {
+            User user = _db.Users.Find(id);
+
+            if (user != null)
+            {
+                _db.Users.Remove(user);
+            }
+        }
+
+        public IEnumerable<User> Find(Func<User, bool> predicate)
+        {
+            return _db.Users.Where(predicate).ToList();
+        }
+
+        public User Get(int id)
+        {
+            User user = _db.Users.Find(id);
+
+            if (user != null)
+            {
+                var query = _db.Users.Include(u => u.Role);
+                return query.Where(u => u.Id == id).First();
+            }
+            else
+                return null;
+        }
+
+        public User Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            var query = _db.Users.Include(u => u.Role);
+            var users = query.ToList();
+
+            if (users.Count == 0)
+                return null;
+
+            return users;
+        }
+
+        public void Update(User item)
+        {
+            _db.ChangeTracker.Clear();
+            _db.Users.Update(item);
+        }
+    }
+}
