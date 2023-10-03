12a20ac63b87b988c7c1382cdc3cb52319271e15
m3mph17 <ez4genius@gmail.com>
2023-10-03 10:13:19 +0300

    users and roles table added, standard admin and user implemented

--- OrganizeYou.DAL/EF/AppDbContext.cs
+++ OrganizeYou.DAL/EF/AppDbContext.cs
@@ -7,6 +7,8 @@ namespace OrganizeYou.DAL.EF
     {
         public DbSet<TaskObject> Tasks { get; set; }
         public DbSet<Status> Statuses { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
@@ -21,7 +23,19 @@ namespace OrganizeYou.DAL.EF
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            string adminRoleName = "admin";
+            string userRoleName = "user";
+
+            string adminEmail = "admin@mail.ru";
+            string adminPassword = "testAdmin2209";
+
+            Role adminRole = new Role { Id = 1, Name = adminRoleName };
+            Role userRole = new Role { Id = 2, Name = userRoleName };
+            User adminUser = new User { Id = 1, Email = adminEmail, Password = adminPassword, RoleId = adminRole.Id };
+
+            modelBuilder.Entity<Role>().HasData(new Role[] { adminRole, userRole });
+            modelBuilder.Entity<User>().HasData(new User[] { adminUser });
+
             modelBuilder.Entity<Status>().HasData(new List<Status>
             {
                 new Status { Id = 1, Name = "Не начата"},
@@ -29,6 +43,8 @@ namespace OrganizeYou.DAL.EF
                 new Status { Id = 3, Name = "Приостановлена"},
                 new Status { Id = 4, Name = "Завершена"},
             });
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
