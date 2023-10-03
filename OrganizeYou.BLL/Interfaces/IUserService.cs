abddffc86ea5a17f8a4e16b7237aa7cb381d574e
m3mph17 <ez4genius@gmail.com>
2023-10-03 13:22:00 +0300

    user service created

---
+++ OrganizeYou.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using OrganizeYou.BLL.DTO;
+using OrganizeYou.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizeYou.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        void CreateUser(UserDTO userDto);
+        UserDTO GetUser(int? id);
+        void DeleteUser(int? id);
+        void UpdateUser(UserDTO userDto);
+        Role GetRole();
+        IEnumerable<UserDTO> GetUsers();
+        void Dispose();
+
+    }
+}
