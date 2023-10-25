340dcdd92c93bf5c22fe034d2d2fa791ed680e2c
m3mph17 <ez4genius@gmail.com>
2023-10-25 12:20:22 +0300

    added test.txt

--- OrganizeYou.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OrganizeYou.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
